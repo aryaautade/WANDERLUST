030e5bd2a033b123694542b762b668916a50ba9f
aryaautade <autadea135@gmail.com>
2025-11-16 23:03:09 +0530

    corrected cloudinaryconfig.js

--- cloudconfig.js
+++ cloudconfig.js
@@ -4,22 +4,23 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
-
+    api_secret: process.env.CLOUD_API_SECRET
 });
+
 
 console.log("SECRET CHECK:", process.env.CLOUD_API_SECRET);
 
-
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
-    params: {
-        folder: 'wanderlust_DEV',
-        allowedFormats: ["png", "jpg", "jpeg"],
-    },
+    params: async (req, file) => {
+        return {
+            folder: "wanderlust_DEV",
+            allowed_formats: ["png", "jpg", "jpeg"]
+        };
+    }
 });
 
 module.exports = {
     cloudinary,
     storage,
-}
+};
