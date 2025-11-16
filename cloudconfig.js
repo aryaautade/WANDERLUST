77dc045da6f8a5bda2988e928dc001408a06efd6
aryaautade <autadea135@gmail.com>
2025-11-16 22:54:21 +0530

    added console

--- cloudconfig.js
+++ cloudconfig.js
@@ -4,8 +4,12 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
+    api_secret: process.env.CLOUD_API_SECRET,
+
 });
+
+console.log("SECRET CHECK:", process.env.CLOUD_API_SECRET);
+
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
